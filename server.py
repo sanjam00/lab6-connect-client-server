dea8b34c5f8badee8b7f05da154d5428e64aa257
Sanaya James <sanaya.j.james@gmail.com>
2026-03-13 21:46:51 -0700

    sestup pipenv and flask. built backend logic for get and post requests

--- server.py
+++ server.py
@@ -6,12 +6,19 @@ CORS(app)
 
 # Create a list called 'events' with a couple of sample event dictionaries
 # Each dictionary should have an 'id' and a 'title'
+events =[{"id": 1, "title": "Summertime Ball"}, {"id": 2, "title": "Work Party"}, {"id": 3, "title": "React Workshop"}]
 
 # TASK: Create a route for "/"
 # This route should return a JSON welcome message
+@app.route("/", methods=["GET"])
+def index():
+	return jsonify({"message": "Welcome!"}), 200
 
 # TASK: Create a GET route for "/events"
 # This route should return the full list of events as JSON
+@app.route("/events", methods=["GET"])
+def get_events():
+	return jsonify(events)
 
 # TASK: Create a POST route for "/events"
 # This route should:
@@ -20,6 +27,14 @@ CORS(app)
 # 3. Create a new event with a unique ID and the provided title
 # 4. Add the new event to the events list
 # 5. Return the new event with status code 201
+@app.route("/events", methods=["POST"])
+def add_events():
+	data = request.get_json()
+	if not "title":
+		return ("Title is required", 404)
+	new_event = {"id": len(events) + 1, "title": data["title"]}
+	events.append(new_event)
+	return jsonify(new_event), 201
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
